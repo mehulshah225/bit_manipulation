54d29097e2ddc471a450ac0b8571548773cdc4bd
mehulshah225 <mehulshah2225@gmail.com>
2023-10-31 14:50:07 -0500

    Toggle bits in C

---
+++ TOGGLE_BITS/README.md
@@ -0,0 +1,5 @@
+# This is a readme for toggle.c
+
+The program intents to toggle 2 bits of a number (signed or unsigned)
+
+
---
+++ TOGGLE_BITS/toggle.c
@@ -0,0 +1,46 @@
+/* The code intents to toogle certain bits of a number (signed or unsigned) */
+/* Author: Mehul Shah */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+void toggleUnsigned(uint32_t* a, int toggle, int toggle2){
+    *a ^= (1u << toggle) | (1u << toggle2);
+    printf("Toggle value: %u\n", *a);
+}   
+
+void toggleSigned(int* a, int toggle, int toggle2){
+    *a ^= (1 << toggle) | (1 << toggle2);
+    printf("Toggle value: %d\n", *a);
+}
+
+int main(int argc, char* argv[]){
+    
+    uint32_t num;
+    int x;
+    int option, toggle1, toggle2;
+
+    printf("Press option:\n1. Signed\n2. Unsigned\n");
+    scanf("%d", &option);
+    printf("Enter bits to be toggled (eg: 6 7): ");
+    scanf("%d %d", &toggle1, & toggle2);
+
+    switch(option){
+        case 1:
+            printf("Enter signed number: ");
+            scanf("%d", &x);
+            toggleSigned(&x, toggle1, toggle2);
+            break;
+        case 2:
+            printf("Enter unsigned number: ");
+            scanf("%u", &num);
+            toggleUnsigned(&num, toggle1, toggle2);
+            break;
+        default:
+            printf("Invalid choice");
+            break;
+    }
+
+    return 0;
+}
